aed1a41508a3b04d8ec1f5571e33dcef311bc8c7
Paul Negoescu <paul.negoescu@nagarro.com>
2024-04-25 17:08:56 +0300

    Pagination implemented

--- src/features/Boardgames/BoardgameList.jsx
+++ src/features/Boardgames/BoardgameList.jsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Card } from './Card';
+import { Pagination } from '@/components/Pagination/Pagination';
 
 const apiUrl = import.meta.env.VITE_API_URL;
+const gamesPerPage = 10;
 
 export function BoardgameList() {
   const [boardgames, setBoardgames] = useState(null);
+  const [numberOfGames, setNumberOfGames] = useState(0);
+
+  const [search] = useSearchParams();
+  const page = search.get('page') ?? 1;
 
   useEffect(() => {
     async function getBoardgames() {
       const data = await fetch(
-        `${apiUrl}/boardgames?_page=1&_per_page=10`
-      ).then((res) => res.json());
+        `${apiUrl}/boardgames?_page=${page}&_limit=${gamesPerPage}`
+      ).then((res) => {
+        const totalNoGames = res.headers.get('x-total-count');
+        setNumberOfGames(totalNoGames);
+        return res.json();
+      });
       setBoardgames(data);
     }
 
     getBoardgames();
-  }, []);
+  }, [page]);
 
   if (!boardgames) {
     return <strong>Loading ...</strong>;
@@ -29,6 +40,10 @@ export function BoardgameList() {
           <Card key={bg.id} game={bg} />
         ))}
       </div>
+      <Pagination
+        numberOfItems={numberOfGames}
+        itemsPerPage={gamesPerPage}
+      />
     </>
   );
 }
